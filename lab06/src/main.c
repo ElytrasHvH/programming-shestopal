27a9891eaf97acabedfcb0d88144127a3d7dbfe5
Дмитро Шестопал <Dmytro.Shestopal@cs.khpi.edu.ua>
2022-10-31 20:23:40 +0100

    low quality product

--- lab06/src/main.c
+++ lab06/src/main.c
@@ -1,34 +1,49 @@
 #include "stdio.h"
 int main()
 {
+	int x = -1;
+	int ii;
+	for (ii = 0; x < 1; ii++) { //kinda infinite loop forcing to make matrix's dimenson positive integer
+		printf("Matrix dimension is: ");
+		scanf("%d", &x);
+		if (x < 1) {
+			printf("Dimension must be at least 1\n");
+		}
+	}
+	if (x == 0) {
+		return x * x;
+	}
+	int i, j, k, a[x][x], b[x][x];
+	printf("\n");
+	for (i = 0; i < x; i++) {
+		for (j = 0; j < x; j++) {
+			a[i][j] = (i + 1) * j + i; //filling matrix with random stuff
+		}
+	}
+	for (i = 0; i < x; i++) {
+		for (j = 0; j < x; j++) {
+			printf("%d ", a[i][j]); //printing matrix that was computed (before squaring)
+		}
+		if (j == x) {
+			printf("\n"); //dropping to next line to see matrix normally
+		}
+	}
+	for (i = 0; i < x; i++) { //for every row
+		for (j = 0; j < x; j++) { //and every column
+			b[i][j] = 0;
+			for (k = 0; k < x; k++) { //k being a counter to get numbers with a look of number(column's number) and (row's number)number
+				b[i][j] += a[i][k] * a[k][j]; //adding up because this how matrix multiplication works
+			}
+		}
+	}
+	for (i = 0; i < x; i++) {
+		for (j = 0; j < x; j++) {
+			printf("%d ", b[i][j]); //printing matrix that was computed (after squaring)
+		}
+		if (j == x) {
+			printf("\n"); //dropping to next line to see matrix normally
+		}
+	}
 
-        int x;
-        printf("Matrix dimenion is: ");
-        scanf("%d", &x);
-        int i,j,k, a[x][x], b[x][x];
-        printf("\n");
-        for (i = 0; i < x; i++) {
-                for (j = 0; j < x; j++) {
-                        a[i][j] = (i+1)*j+i;
-                }
-        }
-        for (i = 0; i < x; i++) {
-                for (j = 0; j < x; j++)
-                        printf("%d ", a[i][j]);
-                if (j == x) {
-                        printf("\n");
-                }
-        }
-        for (i = 0; i < x; i++) {
-                for (j = 0; j < x; j++) {
-                        b[i][j] = 0;
-                        for (k = 0; k < x; k++) {
-                                b[i][j] += a[i][k] * a[k][j];
-                        }
-                }
-        }
-        for (i=0;i<x;i++) {
-            for (j = 0; j<n; j++)
-return 0;
+	return x;
 }
-
